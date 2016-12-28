f4dc36ac350a1d7dc1b8e05b0dfec8f80b06cbe8
伊藤 明大 <ito_akihiro@coreprice.com>
2016-12-28 15:50:11 +0900

    00 add

---
+++ 00.py
@@ -0,0 +1,9 @@
+# _*_ coding: utf-8 _*_
+
+char_list = list("stressed")
+#print(''.join(char_list.reverse()))
+#char_list.reverse()
+#print(''.join(char_list))
+
+# 下記でもOK
+print(''.join(char_list[::-1]))
